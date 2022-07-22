c4ce5924951d390a65379a96e956af20194caa58
José Vitor <jose81073175@gmail.com>
2022-07-22 14:47:38 -0300

    refactor: change profile model

--- Models/Profile.cs
+++ Models/Profile.cs
@@ -8,6 +8,12 @@ namespace book_collection.Models
   [Table("profiles")]
   public class Profile
   {
+    public Profile()
+    {
+      PublishingCompanies = new Collection<PublishingCompanie>();
+      ImageProfiles = new Collection<ImageProfile>();
+    }
+
     [Key]
     public int id { get; set; }
 
@@ -16,14 +22,10 @@ namespace book_collection.Models
     public string cpf { get; set; }
 
     [Required]
-    public byte[] image { get; set; }
-
-    [Required]
     [MaxLength(80)]
     public string name { get; set; }
 
     [Required]
-    [MaxLength(80)]
     public DateTime birth_date { get; set; }
 
     [Required]
@@ -33,5 +35,9 @@ namespace book_collection.Models
     [Required]
     [MaxLength(80)]
     public string password { get; set; }
+
+    public ICollection<PublishingCompanie> PublishingCompanies { get; set; }
+
+    public ICollection<ImageProfile> ImageProfiles { get; set; }
   }
 }
