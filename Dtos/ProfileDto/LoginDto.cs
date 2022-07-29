8883adeacef5dc500c005d984193f10b29a0ead3
José Vitor <jose81073175@gmail.com>
2022-07-29 12:52:51 -0300

    feat: create loginDto

---
+++ Dtos/ProfileDto/LoginDto.cs
@@ -0,0 +1,11 @@
+namespace book_collection.Dto
+{
+  public class LoginDto 
+  {
+    public string? cpf { get; set; }
+
+    public string? email { get; set; }
+    
+    public string password { get; set; }
+  }
+}
