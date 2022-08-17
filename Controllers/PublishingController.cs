46e5feca50d2bb668fb576ae89ce02c82293bab3
José Vitor <jose81073175@gmail.com>
2022-08-17 00:31:23 -0300

    feat: using swagger with JWT Bearer token

---
+++ Controllers/PublishingController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using book_collection.Models;
+using book_collection.Context;
+
+namespace book_collection.Controllers;
+
+[ApiController]
+
+[Route("[controller]")]
+public class PublishingController : ControllerBase
+{
+  private readonly AppDbContext _context;
+
+  public PublishingController(AppDbContext context)
+  {
+    this._context = context;
+  }
+
+  [HttpPost]
+  public ActionResult<PublishingCompanie> Post([FromBody] PublishingCompanie publishing)
+
+  {
+    try 
+    {
+      _context.PublishingCompanies.Add(publishing);
+      _context.SaveChanges();
+      return Ok(publishing);
+    }
+    catch (Exception)
+    {
+      return StatusCode(StatusCodes.Status500InternalServerError, "error when registering a new Publishing Companie");
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -84,6 +84,30 @@ public class Program
             Url = new Uri("https://mock.com/license")
         }
       });
+      c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme() 
+      { 
+        Name = "Authorization", 
+        Type = SecuritySchemeType.ApiKey, 
+        Scheme = "Bearer", 
+        BearerFormat = "JWT", 
+        In = ParameterLocation.Header, 
+        Description = "Bearer [space] and then your token in the text input below."+
+        "\r\n\r\nExample: \"Bearer 12345abcdef\"",
+      }); 
+      c.AddSecurityRequirement(new OpenApiSecurityRequirement 
+      { 
+        { 
+          new OpenApiSecurityScheme 
+          { 
+            Reference = new OpenApiReference 
+            { 
+              Type = ReferenceType.SecurityScheme, 
+              Id = "Bearer" 
+            } 
+          }, 
+        new string[] {} 
+        } 
+      }); 
     });
 
     var app = builder.Build();
